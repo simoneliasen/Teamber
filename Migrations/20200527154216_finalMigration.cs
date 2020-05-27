8d3d45341f14e94d885a49766590f31abc1dd83e
Thomas Vinther <tvinther1@live.dk>
2020-05-27 17:45:56 +0200

    ny migration, og fixet fejl med overflow scroll

---
+++ Migrations/20200527154216_finalMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Teamber.Migrations
+{
+    public partial class finalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
